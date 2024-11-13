9019864e6ca888a780f7bb6ec001cf1c512d62fd
Benedict <brjw@hotmail.co.uk>
2024-11-13 16:51:06 +0000

    version bump

--- test/unittest/test_action_source.py
+++ test/unittest/test_action_source.py
@@ -1,7 +1,7 @@
 """Test case for action source - the source of an actio is composed of the ID of the agent and the executing component, this should be recoverable."""
 
 import unittest
-from star_ray.agent import Agent, Sensor, Component
+from star_ray.agent import Agent, Sensor, Actuator, Component
 from star_ray.event import Action
 
 
@@ -17,12 +17,20 @@ class TestActionId(unittest.TestCase):
     def test_action_source(self):
         """Test action source."""
         sensor = Sensor()
+        actuator = Actuator()
         agent = MyAgent([], [])
         agent.add_component(sensor)
+        agent.add_component(actuator)
         action = Action()
         Component.set_action_source(sensor, [action])
         component_id, agent_id = Component.unpack_source(action)
         self.assertEqual(component_id, sensor.id)
+        self.assertEqual(agent_id, agent.id)
+
+        action = Action()
+        Component.set_action_source(actuator, [action])
+        component_id, agent_id = Component.unpack_source(action)
+        self.assertEqual(component_id, actuator.id)
         self.assertEqual(agent_id, agent.id)
 
 
